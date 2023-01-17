62fe535723a7678b7de7c96ca23a181e57c7541b
canmert <canmert.dev@gmail.com>
2023-01-17 19:39:44 +0300

    feat: adding data of items

---
+++ src/data/items.ts
@@ -0,0 +1,59 @@
+import pancakageImg from "../assets/images/butter-pancakes.png";
+import sweetYogurtImg from "../assets/images/sweet-yogurt.png";
+import avocadoToastImg from "../assets/images/avocado-toast.png";
+import peanutToastImg from "../assets/images/peanut-toast.png";
+import frenchToastImg from "../assets/images/french-toast.png";
+import bananaBreadImg from "../assets/images/banana-bread.png";
+import classicWaffleImg from "../assets/images/classic-waffle.png";
+
+export const items = [
+  {
+    id: 1,
+    name: "Butter Pancakes",
+    caption: "With",
+    price: 14.5,
+    image: pancakageImg,
+  },
+  {
+    id: 2,
+    name: "Avocado Toast",
+    caption: "2 pieces",
+    price: 16.5,
+    image: avocadoToastImg,
+  },
+  {
+    id: 3,
+    name: "Sweet Yogurt",
+    caption: "With strawberries",
+    price: 5.0,
+    image: sweetYogurtImg,
+  },
+  {
+    id: 4,
+    name: "Peanut Toast",
+    caption: "With butter",
+    price: 9.5,
+    image: peanutToastImg,
+  },
+  {
+    id: 5,
+    name: "French Toast",
+    caption: "With butter and jam",
+    price: 9.5,
+    image: frenchToastImg,
+  },
+  {
+    id: 6,
+    name: "Banana Bread",
+    caption: "With blueberries",
+    price: 5.5,
+    image: bananaBreadImg,
+  },
+  {
+    id: 7,
+    name: "Classic Waffle",
+    caption: "With ice cream",
+    price: 7.5,
+    image: classicWaffleImg,
+  },
+];
